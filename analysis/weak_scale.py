18f442dd00262ca368cb072785ded1b5c7bf3f18
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-20 20:32:14 +0100

    Upload compiling scripts

--- analysis/weak_scale.py
+++ analysis/weak_scale.py
@@ -1,6 +1,7 @@
 import os
 import unyt
 import numpy as np
+from typing import Tuple
 from matplotlib import pyplot as plt
 
 try:
@@ -32,15 +33,31 @@ def number_of_threads(run_directory: str) -> int:
     threads_per_tile = dir_basename.split('_')[-1][0]
     return int(tile_x) * int(tile_y) * int(threads_per_tile)
 
+def weak_scale_omp(threads_per_tile: int = 1) -> Tuple[np.ndarray]:
+    # Open-MP runs directory
+    omp_dir = '/cosma6/data/dp004/dc-alta2/exascale-hydro/kelvin-helmholtz-2D/omp'
+    omp_runs = [os.path.join(omp_dir, i) for i in os.listdir(omp_dir) if
+                os.path.isdir(os.path.join(omp_dir, i)) and i.endswith(str(threads_per_tile))]
+    threads = []
+    time2sol = []
+    for run in omp_runs:
+        threads.append(number_of_threads(run))
+        time2sol.append(time_to_solution(run))
+
+    threads = np.asarray(threads, dtype=np.int)
+    time2sol = np.asarray(time2sol, dtype=np.float)
+
+    sort_key = np.argsort(threads)
+    return threads[sort_key], time2sol[sort_key]
+
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-# Open-MP runs directory
-omp_dir = '/cosma6/data/dp004/dc-alta2/exascale-hydro/kelvin-helmholtz-2D/omp'
-omp_runs = [os.path.join(omp_dir, i) for i in os.listdir(omp_dir) if os.path.isdir(os.path.join(omp_dir, i)) and i.endswith('4')]
-for run in omp_runs:
-    print(number_of_threads(run), time_to_solution(run))
-    ax.scatter(number_of_threads(run), time_to_solution(run), c='k')
+for n in range(1,5):
+    threads, time2sol = weak_scale_omp(threads_per_tile=n)
+    ax.plot(threads, time2sol, label=f"threads_per_tile = {n}")
+
 
 ax.grid(linestyle='--', color='grey', linewidth=0.5)
 ax.set_xlabel('Number of threads')
@@ -49,6 +66,7 @@ ax.set_ylim([0, 1])
 ax.axvspan(16, ax.get_xlim()[-1], alpha=0.25, facecolor='red')
 ax.text(17, 0.1, "Hyper-threading", color="grey", ha="left", va="bottom")
 ax.axhline(1, linestyle='-', color='black', alpha=0.1)
+plt.legend()
 fig.tight_layout()
 plt.show()
 # plt.savefig(os.path.join(basepath, 'wallclocktime.png'), to_slack=True, dpi=400)
