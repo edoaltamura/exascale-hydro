f94c251237467c5e53830733990f3922d060c91b
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-20 17:13:33 +0100

    Upload compiling scripts

--- kelvin-helmholtz/setup_kh2d.sh
+++ kelvin-helmholtz/setup_kh2d.sh
@@ -11,7 +11,7 @@ old_directory=$(pwd)
 # Set-up run | you can change these values
 architecture="mpi"
 resolution=512
-tiling="4x4"
+tiling="5x5"
 threads_per_tile=4
 
 # Set-up exa-scale project directories
