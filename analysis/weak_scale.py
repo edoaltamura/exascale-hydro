69291dc0d3fd0ecbebd5fcd778f98517800ececa
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-20 20:16:56 +0100

    Upload compiling scripts

--- analysis/weak_scale.py
+++ analysis/weak_scale.py
@@ -34,10 +34,6 @@ def number_of_threads(run_directory: str) -> int:
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_xlabel('Number of threads')
-ax.set_ylabel('Time to solution [hours]')
-ax.axvspan(16, ax.get_xlim()[-1], alpha=0.25, facecolor='red')
-ax.axhline(1, linestyle='-', color='black')
 
 # Open-MP runs directory
 omp_dir = '/cosma6/data/dp004/dc-alta2/exascale-hydro/kelvin-helmholtz-2D/omp'
@@ -46,8 +42,11 @@ for run in omp_runs:
     print(number_of_threads(run), time_to_solution(run))
     ax.scatter(number_of_threads(run), time_to_solution(run))
 
-
 ax.grid(linestyle='--', color='grey', linewidth=0.5)
+ax.set_xlabel('Number of threads')
+ax.set_ylabel('Time to solution [hours]')
+ax.axvspan(16, ax.get_xlim()[-1], alpha=0.25, facecolor='red')
+ax.axhline(1, linestyle='-', color='black')
 fig.tight_layout()
 plt.show()
 # plt.savefig(os.path.join(basepath, 'wallclocktime.png'), to_slack=True, dpi=400)
