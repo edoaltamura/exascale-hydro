0ced4f627c4af2edd932659c9424f9e8747a55e0
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-30 17:35:06 +0100

    Implement low RAM footprint ICs generation module. The old version is still kept for comparison.

--- kelvin-helmholtz/setup_run_3d.sh
+++ kelvin-helmholtz/setup_run_3d.sh
@@ -64,7 +64,7 @@ setup_run(){
   sed -i "s/MAX_TOP_CELLS/$total_top_cells/" ./param.yml
 
   # Edit mutable parameters in the submit file
-  cat > submit.slurm << 'EOF'
+  cat > submit.slurm << EOF
 #!/bin/bash -l
 
 #SBATCH -N $nodes
@@ -93,11 +93,11 @@ mpirun -np $(( $tiles * $tiles * $tiles )) \
     --hydro \
     -v 1 \
     --pin \
-    --threads=$threads_per_tile ./param.yml
+    --threads=\$SLURM_CPUS_PER_TASK ./param.yml
 
 
 echo "Job done, info follows."
-sacct -j $SLURM_JOBID --format=JobID,JobName,Partition,AveRSS,MaxRSS,AveVMSize,MaxVMSize,Elapsed,ExitCode
+sacct -j \$SLURM_JOBID --format=JobID,JobName,Partition,AveRSS,MaxRSS,AveVMSize,MaxVMSize,Elapsed,ExitCode
 EOF
 
   # Generate the output list with times in the future
