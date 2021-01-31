82d8bc105a50e01e19fa3edc6d37c8bb43202f09
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-31 11:26:32 +0100

    Implement low RAM footprint ICs generation module. The old version is still kept for comparison.

--- kelvin-helmholtz/setup_run_3d.sh
+++ kelvin-helmholtz/setup_run_3d.sh
@@ -119,14 +119,19 @@ EOF
 
 }
 
-nohup setup_run 128 5 14 5 &
-nohup setup_run 128 6 14 5 &
-nohup setup_run 128 7 14 5 &
+
+setup_run 128 5 14 5 &
+setup_run 128 6 14 5 &
+setup_run 128 7 14 5 &
 #setup_run 128 8 14 5
 #setup_run 128 9 14 5
 
-nohup setup_run 256 5 14 5 &
-nohup setup_run 256 6 14 5 &
-nohup setup_run 256 7 14 5 &
+setup_run 256 5 14 5 &
+setup_run 256 6 14 5 &
+setup_run 256 7 14 5 &
 #setup_run 256 8 14 5
 #setup_run 256 9 14 5
+
+wait
+echo "All done!"
+exit 0
