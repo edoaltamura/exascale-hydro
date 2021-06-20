3a5e7d0a1f0169689756bf49c2a0d7ad8697c3ac
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-20 23:10:12 +0100

    Analysis tool for stdout

--- analysis/weak_scale.py
+++ analysis/weak_scale.py
@@ -5,10 +5,11 @@ from analyse_stdout import Stdout
 
 plt.style.use('../mnras.mplstyle')
 
-cwd = '/cosma8/data/dr004/dc-alta2'#/with_intelmpi2020u2'
+cwd = '/cosma8/data/dr004/dc-alta2'  # /with_intelmpi2020u2'
 threads_per_node = 128
 __particle_load = 256
 __ranks_per_node = 4
+plot_annotations = False
 
 
 def get_stdout_path(
@@ -41,104 +42,112 @@ tiling_orders = {
     8: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14]
 }
 
-logs = []
-for t in tiling_orders[__ranks_per_node]:
-    logs.append(get_stdout_path(
-        __ranks_per_node, __particle_load, t, threads_per_node / __ranks_per_node
-    ))
-
-good_timesteps = []
-no_clean_steps = []
-for i, log in enumerate(logs):
-    print(log)
-    test = Stdout(os.path.join(cwd, log))
-    timestep_number, particle_updates, timestep_duration, timestep_properties = test.analyse_stdout()
-
-    if len(timestep_number) > 0:
-        is_clean = np.logical_and(
-            timestep_properties == 0,
-            particle_updates == particle_updates[0]
-        )
-
-        if len(timestep_number[is_clean]) > 0:
-            good_timesteps.append(
-                list(timestep_number[is_clean])
-            )
-        else:
-            no_clean_steps.append(log)
-
-    else:
-        no_clean_steps.append(log)
-
-common_timesteps = list(set(good_timesteps[0]).intersection(*good_timesteps[1:]))
-common_timesteps = np.asarray(common_timesteps)
-print('common timesteps', common_timesteps)
-
-print('runs with no clean timesteps:', no_clean_steps)
-logs = [log for log in logs if log not in no_clean_steps]
-
-particles = np.empty(len(logs))
-ranks = np.empty(len(logs))
-threads = np.empty(len(logs))
-times_mean = np.empty(len(logs))
-times_std = np.empty(len(logs))
-time_per_update = np.empty(len(logs))
-
-for i, log in enumerate(logs):
-    test = Stdout(os.path.join(cwd, log))
-
-    particles[i] = test.num_particles()
-    ranks[i] = test.num_ranks()
-    threads[i] = test.num_ranks() * test.threads_per_rank()
-
-    timestep_number, particle_updates, timestep_duration, timestep_properties = test.analyse_stdout()
-    timestep_number = timestep_number[common_timesteps]
-    particle_updates = particle_updates[common_timesteps]
-    timestep_duration = timestep_duration[common_timesteps]
-    timestep_properties = timestep_properties[common_timesteps]
-
-    is_clean = np.logical_and(
-        timestep_properties == 0,
-        particle_updates == particle_updates[0],
-    )
-    times_mean[i] = timestep_duration[is_clean].mean().to('microsecond')
-    times_std[i] = timestep_duration[is_clean].std().to('microsecond')
-    time_per_update[i] = times_mean[i] * threads[i] / particles[i]  # micro-second
-
-print('particles', particles)
-print('ranks', ranks)
-print('threads', threads)
-print('times_mean', times_mean)
-
+# Setup figure structure
 fig, axes = plt.subplots()
 axes.axhline(1, lw=0.5, ls='--', c='lightgrey')
-axes.errorbar(
-    threads,
-    times_mean / times_mean[0],
-    yerr=times_std / times_mean[0],
-    lw=0.5, marker='o', markersize=2,
-    label=''
-)
 axes.set_xlabel('Cores [-]')
 axes.set_ylabel('Parallel efficiency relative to $2^3$ tiles [-]')
 axes.set_xscale('log')
 axes.set_xlim(threads_per_node / 2, 5e5)
 axes.set_ylim(0, 1.5)
-
 ax_nodes = axes.twiny()
 ax_nodes.set_xscale("log")
 ax_nodes.set_xlim(0.5, 5e5 / threads_per_node)
 ax_nodes.set_xlabel("Nodes [-]")
-
 ax_partupdate = axes.twinx()
-ax_partupdate.set_ylim(0 * np.median(time_per_update), 1.5 * np.median(time_per_update))
 ax_partupdate.set_ylabel("Time to update one particle [$\\mu$s]", labelpad=4)
 
-for i in range(len(threads)):
-    exponent = np.floor(np.log10(particles[i]))
-    mantissa = np.floor(particles[i] / 10 ** (exponent - 2)) / 100.
-    axes.text(threads[i], times_mean[i] / times_mean[0] - 0.14,
-              "$N_p = %.2f\\times10^{%d}~\\rightarrow$" % (mantissa, exponent),
-              rotation=90, va="top", fontsize=4, color='C0', ha="center", backgroundcolor='none')
+for __ranks_per_node in [2, 4, 8]:
+
+    logs = []
+    for t in tiling_orders[__ranks_per_node]:
+        logs.append(get_stdout_path(
+            __ranks_per_node, __particle_load, t, threads_per_node / __ranks_per_node
+        ))
+
+    good_timesteps = []
+    no_clean_steps = []
+    for i, log in enumerate(logs):
+        print(log)
+        test = Stdout(os.path.join(cwd, log))
+        timestep_number, particle_updates, timestep_duration, timestep_properties = test.analyse_stdout()
+
+        if len(timestep_number) > 0:
+            is_clean = np.logical_and(
+                timestep_properties == 0,
+                particle_updates == particle_updates[0]
+            )
+
+            if len(timestep_number[is_clean]) > 0:
+                good_timesteps.append(
+                    list(timestep_number[is_clean])
+                )
+            else:
+                no_clean_steps.append(log)
+
+        else:
+            no_clean_steps.append(log)
+
+    common_timesteps = list(set(good_timesteps[0]).intersection(*good_timesteps[1:]))
+    common_timesteps = np.asarray(common_timesteps)
+    print('common timesteps', common_timesteps)
+
+    print('runs with no clean timesteps:', no_clean_steps)
+    logs = [log for log in logs if log not in no_clean_steps]
+
+    particles = np.empty(len(logs))
+    ranks = np.empty(len(logs))
+    threads = np.empty(len(logs))
+    times_mean = np.empty(len(logs))
+    times_std = np.empty(len(logs))
+    time_per_update = np.empty(len(logs))
+
+    for i, log in enumerate(logs):
+        test = Stdout(os.path.join(cwd, log))
+
+        particles[i] = test.num_particles()
+        ranks[i] = test.num_ranks()
+        threads[i] = test.num_ranks() * test.threads_per_rank()
+
+        timestep_number, particle_updates, timestep_duration, timestep_properties = test.analyse_stdout()
+        timestep_number = timestep_number[common_timesteps]
+        particle_updates = particle_updates[common_timesteps]
+        timestep_duration = timestep_duration[common_timesteps]
+        timestep_properties = timestep_properties[common_timesteps]
+
+        is_clean = np.logical_and(
+            timestep_properties == 0,
+            particle_updates == particle_updates[0],
+        )
+        times_mean[i] = timestep_duration[is_clean].mean().to('microsecond')
+        times_std[i] = timestep_duration[is_clean].std().to('microsecond')
+        time_per_update[i] = times_mean[i] * threads[i] / particles[i]  # micro-second
+
+    print('particles', particles)
+    print('ranks', ranks)
+    print('threads', threads)
+    print('times_mean', times_mean)
+
+
+    axes.errorbar(
+        threads,
+        times_mean / times_mean[0],
+        yerr=times_std / times_mean[0],
+        lw=0.5, marker='o', markersize=2,
+        label=''
+    )
+
+    ax_partupdate.set_ylim(0 * np.median(time_per_update), 1.5 * np.median(time_per_update))
+
+    if plot_annotations:
+        for i in range(len(threads)):
+            exponent = np.floor(np.log10(particles[i]))
+            mantissa = np.floor(particles[i] / 10 ** (exponent - 2)) / 100.
+            axes.text(
+                threads[i],
+                times_mean[i] / times_mean[0] - 0.14,
+                "$N_p = %.2f\\times10^{%d}~\\rightarrow$" % (mantissa, exponent),
+                rotation=90, va="top", fontsize=4, color='C0', ha="center", backgroundcolor='none'
+            )
 
 plt.show()
